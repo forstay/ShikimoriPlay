a6d604164d715749f178b6ff57031ae051564862
upomoew <upomoew@gmail.com>
2017-07-18 23:03:53 +0200

    ES6

--- inject.js
+++ inject.js
@@ -28,6 +28,6 @@ function showPlayButton() {
 }
 
 function isNormalInteger(str) {
-	var n = Math.floor(Number(str));
-	return String(n) === str && n >= 0;
+  var n = Math.floor(Number(str))
+  return String(n) === str && n >= 0
 }
